8984958231de3c5bfb94bd968096f5ce41d31d0c
Talal Dalla <talaldalla@hotmail.com>
2013-08-23 13:47:23 +0300

    Boxes Animation

---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+	
+	public Transform Targetpoint;
+	
+	public NavMeshAgent myAg;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		myAg.SetDestination(Targetpoint.position);
+	}
+}
